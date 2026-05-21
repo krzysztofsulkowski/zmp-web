c84bcdbb6367c706bb9a25962372f72b1e495566
Elleonora13414 <ntwlczk@gmail.com>
2026-05-21 23:57:31 +0200

    updated admin panel visuals

--- GameShelf.Web/src/app/admin/page.tsx
+++ GameShelf.Web/src/app/admin/page.tsx
@@ -1,17 +1,16 @@
 import { useNavigate } from 'react-router-dom';
 import styles from './Admin.module.css';
 import logo from '@/assets/logo.svg';
+import arrow from '@/assets/arrow-right-black.svg';
+
 
 export default function AdminPage() {
     const navigate = useNavigate();
 
     return (
         <main className={styles.page}>
-            <img
-                src={logo}
-                alt="GameShelf"
-                className={styles.logo}
-            />
+            <img src={logo} alt="GameShelf" className={styles.logo} />
+
             <button
                 className={styles.logoutButton}
                 onClick={() => {
@@ -25,21 +24,18 @@ export default function AdminPage() {
             <section className={styles.card}>
                 <h1>Panel administratora</h1>
 
-                <p>
-                    Jeśli widzisz tę stronę, oznacza to, że konto ma rolę administratora.
-                </p>
-
                 <div className={styles.buttons}>
-                    <button onClick={() => navigate('/logs')}>
+                    <button className={styles.adminButton} onClick={() => navigate('/logs')}>
                         Logi systemowe
+                        <img src={arrow} alt="" width={25} />
                     </button>
-
-                    <button onClick={() => navigate('/admin-games')}>
+                    <button className={styles.adminButton} onClick={() => navigate('/admin-games')}>
                         Gry
+                        <img src={arrow} alt="" width={25} />
                     </button>
-
-                    <button onClick={() => navigate('/users')}>
+                    <button className={styles.adminButton} onClick={() => navigate('/users')}>
                         Użytkownicy
+                        <img src={arrow} alt="" width={25} />
                     </button>
                 </div>
             </section>
